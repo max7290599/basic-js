1e228a0d5fa48a60044d9acc44bf7d1d13ec05ef
max7290599 <atroshschanka.maksim@gmail.com>
2021-03-14 22:34:29 +0300

    feat: add function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+     function radioactiveDecay() {
+         return Math.ceil(Math.log(2) / HALF_LIFE_PERIOD * 1e8) / 1e4;
+    }
+
+    module.exports = function dateSample(sampleActivity) {
+        if (typeof(sampleActivity) !== 'string' || !sampleActivity || !isFinite(sampleActivity)) {
+            return false;
+        } else {
+            let number = Number(sampleActivity);
+            if (isNaN(number)|| number <= 0) {
+
+                return false
+            } else {
+
+                return Math.ceil(((Math.log(MODERN_ACTIVITY / number)) / 0.00012094))
+            }
+        }
+
+    };
+
