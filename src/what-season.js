1158c77a0648b3d1dc95f109e42d3c3c4a8662e1
max7290599 <atroshschanka.maksim@gmail.com>
2021-03-22 10:38:29 +0300

    refactor: fix null

--- src/what-season.js
+++ src/what-season.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
 
-  if (date === null) {
+  if (date == null) {
     return 'Unable to determine the time of year!'
   }
 
