2dac0248cd58cb15cfb797d4fe653331d8d6a242
max7290599 <atroshschanka.maksim@gmail.com>
2021-03-22 10:32:08 +0300

    fix: add if

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,21 +3,21 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730
 
-     function radioactiveDecay() {
-         return Math.ceil(Math.log(2) / HALF_LIFE_PERIOD * 1e8) / 1e4;
-    }
+
 
     module.exports = function dateSample(sampleActivity) {
-        if (typeof(sampleActivity) !== 'string' || !sampleActivity || !isFinite(sampleActivity)) {
+        if (!sampleActivity || typeof(sampleActivity) !== 'string' || !isFinite(sampleActivity)) {
             return false;
         } else {
             let number = Number(sampleActivity);
-            if (isNaN(number)|| number <= 0) {
 
-                return false
+            if (isNaN(number)|| number <= 0 || number>= 15) {
+
+                return false;
             } else {
+                let izotop = Math.log(MODERN_ACTIVITY/sampleActivity);
 
-                return Math.ceil(((Math.log(MODERN_ACTIVITY / number)) / 0.00012094))
+                return Math.ceil(izotop/(0.693/HALF_LIFE_PERIOD));
             }
         }
 
